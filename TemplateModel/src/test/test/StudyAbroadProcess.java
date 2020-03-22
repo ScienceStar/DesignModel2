5a8175c794f4b52cc337c34fb1aa5d993d255287
sciencestar <1633696870@qq.com>
2020-03-22 18:18:34 +0800

    模板模式被调用

---
+++ TemplateModel/src/test/test/StudyAbroadProcess.java
@@ -0,0 +1,16 @@
+import abst.StudyAbroad;
+import automatic.StudyInAmerica;
+
+/**
+ * @ClassName StudyAbroadProcess
+ * @Description: TODO
+ * @Author lxc
+ * @Date 2020/3/22 18:16
+ * @Version V1.0
+ **/
+public class StudyAbroadProcess {
+    public static void main(String[] args) {
+        StudyAbroad tm=new StudyInAmerica();
+        tm.TemplateMethod();
+    }
+}
